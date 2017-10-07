15f03866ee0164b557a61886c917c0fccb6b4647
Marcus Karpoff <mkarpoff@ualberta.ca>
2017-10-07 11:59:13 -0600

    Made linkall.py use a json file for portability

--- linkall.py
+++ linkall.py
@@ -1,43 +1,38 @@
 #! /usr/bin/env python3
 
 from pathlib import Path
+import json
 import sys
+if sys.version_info[0] < 3 or sys.version_info[1] < 5:
+    raise "Must be greater than or equal to Python 3.5 to run this script"
 name="name"
 target="target"
 
-files=[
-    {name:"vim-pack",         target:"~/.local/bin/vim-pack"},
-    {name:"vim-pack-get",     target:"~/.local/bin/vim-pack-get"},
-    {name:"vim-pack-remove",  target:"~/.local/bin/vim-pack-remove"},
-    {name:"vim-pack-update",  target:"~/.local/bin/vim-pack-update"},
-    {name:"vim-pack-upgrade",  target:"~/.local/bin/vim-pack-upgrade"},
-    {name:"vim-pack-export",  target:"~/.local/bin/vim-pack-export"},
-    {name:"vim-pack-import",  target:"~/.local/bin/vim-pack-import"},
-    {name:"bash_completion",  target:"~/.bash_completion"},
-    {name:"vim-pack-complete",target:"~/.bash_completion.d/vim-pack-complete"},
-    ]
+with open("files.json") as data_files:
+    files = json.load(data_files)
+
 
 def link(filename, target):
     target_path = Path(target).expanduser()
     source_path = Path(filename).expanduser()
     if (not source_path.exists()):
-        print("[%16s] -> [%s] Not linked. Source missing" %(source_path, target_path))
+        print("[%-30s] -> [%-40s] Not linked. Source missing" %(source_path, target_path))
         return
     if (target_path.exists()):
-        print("[%16s] -> [%s] Not linked. Target exists" %(source_path, target_path))
+        print("[%-30s] -> [%-40s] Not linked. Target exists" %(source_path, target_path))
         return
     if (target_path.is_symlink()):
-        print("[%16s] -> [%s] Target exist and is symlink" %(source_path, target_path))
+        print("[%-30s] -> [%-40s] Target exist and is symlink" %(source_path, target_path))
         if (query_yes_no("Remove and remake? ")):
             target_path.unlink()
-            print("[%16s] -> [%s] Target old link removed" %(source_path, target_path))
+            print("[%-30s] -> [%-40s] Target old link removed" %(source_path, target_path))
         else:
             return
     if (not target_path.parent.exists()):
-        print("[%16s] -> [%s] Creating missing parent directory" %(source_path, target_path))
+        print("[%-30s] -> [%-40s] Creating missing parent directory" %(source_path, target_path))
         target_path.parent.mkdir(parents=True);
     target_path.symlink_to(source_path.resolve())
-    print("[%16s] -> [%s] symlink created successfully" %(source_path, target_path))
+    print("[%-30s] -> [%-40s] Symlink created successfully" %(source_path, target_path))
 
 
 def query_yes_no(question, default="yes"):
